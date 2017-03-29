b22c92296dfe7b936ff859cbf31c63daaaea831d
Stephane Rodet <stephane.rodet@gmail.com>
2017-03-29 22:25:43 +0200

    Improving the typography section for clarity

--- src/components/Typography.vue
+++ src/components/Typography.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="typography">
-    <h1>h1: {{ msg }}</h1>
+    <h2>{{ msg }}</h2>
     <p>
       This page contains both an example of the typography and the explanation
       for the choices made.
     </p>
-    <h2>h2: Heading 2 example</h2>
-    <h3>h3: Heading 3 example</h3>
-    <h4>h4: Heading 4 example</h4>
+    <section class='inverted'>
+      <h1>h1: Heading 1 example</h1>
+      <h2>h2: Heading 2 example</h2>
+      <h3>h3: Heading 3 example</h3>
+      <h4>h4: Heading 4 example</h4>
+    </section>
     <p>
       The scale used for the title font sizes is the Perfect Fourth.
       Starting with a base font size of 16px, a `h4` tag has a size of 1.333em,
-      h3 1.777em, h2 2.369em and h1 3.157.
+      h3 1.777em, h2 2.369em and h1 3.157. All headers have a margin of 1.5em
+      above them to guarantee them enough space, so they can be detached
+      from the former content in a consistent way.
     </p>
     <p>
       The font stack prioritizes performance and platform support. This is
@@ -34,3 +39,18 @@ export default {
   },
 };
 </script>
+
+<style>
+.inverted {
+  overflow:auto;
+  background-color: #2e3673;
+  color: white;
+}
+
+.inverted h1,
+.inverted h2,
+.inverted h3,
+.inverted h4 {
+  color: white;
+}
+</style>
