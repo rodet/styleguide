1aaff6edef9c6777c5bfc90c85910a34cf25ddf5
Stephane Rodet <stephane.rodet@gmail.com>
2017-03-31 21:29:59 +0200

    Show multiple columns layout

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -8,24 +8,25 @@
     <div class='layout__arrow'>
       &harr;
     </div>
-    <p>
-      It's fine to be bold and use the whole window width when needed:
-    </p>
-    <section class='layout__full-width'>
-      <div class='layout__3col'>
-        <h2>Lorem Ipsum 1</h2>
-      </div>
-      <div class='layout__3col'>
-        <h2>Lorem Ipsum 2</h2>
-      </div>
-      <div class='layout__3col'>
-        <h2>Lorem Ipsum 3</h2>
+    <section class='layout--full-width layout--light-blue'>
+      <div class='layout--2col'>
+        <h2 class='layout--light-blue'>Lorem Ipsum 1</h2>
+        <p>
+          It's fine to be bold and use the whole window width when needed.
+          Here also is an example of a multiple columns page. This allows
+          much more interesting layouts, with pictures, text and much more.
+          Such layouts should be preferably exactly one window high, so no
+          scrolling is involved while reading through it.
+        </p>
+        <p>
+          The multiple columns should be used for graphical elements,
+          animations and longer texts.
+          Broad layout can also be used together with high color contrast to
+          draw the reader's attention.
+        </p>
       </div>
     </section>
-    <p>
-      Broad layout can also be used together with high color contrast to
-      draw the reader's attention.
-    </p>
+
   </div>
 </template>
 
@@ -40,25 +41,29 @@ export default {
   font-size: 2.369em;
 }
 
-.layout__full-width {
+.layout--full-width {
   margin-left: calc(50% - 50vw);
   width: 100vw;
   min-height: 200px;
-  background-color: #428bca;
 }
 
-/* Make this more generic */
-.layout__full-width h2 {
+.layout--light-blue {
+  background-color: #428bca;
   color: white;
 }
 
-.layout__3col {
-  width: 33%;
-  display: inline-block;
-  height: 100%;
+.layout--2col,
+.layout--3col {
+  padding: 40px;
+  margin: 50px 0;
 }
 
-.layout__3col + .layout__3col {
-  /*border-left: 1px solid white;*/
+.layout--2col {
+  column-count: 2;
 }
+
+.layout--3col {
+  column-count: 3;
+}
+
 </style>
