8c9a3feb9218b1b2c5e43562e551f7e3187efdbe
Stephane Rodet <stephane.rodet@gmail.com>
2017-04-06 21:43:37 +0200

    New section on colors

---
+++ src/components/Colors.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="colors">
+    <h2>Colors</h2>
+    <p>
+      Colors set the mood and the appearance of a page. We wanted something
+      modern and dynamic, basing on blue for the calmness and clarity,
+      and purple for the contrast and dynamism.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'colors',
+};
+</script>
