390c66f9d93a3b9ebe1e68e5a1cd2129a9619a93
Stephane Rodet <stephane.rodet@gmail.com>
2020-04-06 22:03:52 +0200

    Update eslint rules and adapt code

--- src/components/Typography.vue
+++ src/components/Typography.vue
@@ -5,23 +5,23 @@
       This page contains both an example of the typography and the explanation
       for the choices made.
     </p>
-    <section class='inverted'>
+    <section class="inverted">
       <h1>h1: Heading 1 example</h1>
       <h2>h2: Heading 2 example</h2>
       <h3>h3: Heading 3 example</h3>
       <h4>h4: Heading 4 example</h4>
     </section>
     <p>
-      The scale used for the title font sizes is the Perfect Fourth.
-      Starting with a base font size of 16px, a `h4` tag has a size of 1.333em,
-      h3 1.777em, h2 2.369em and h1 4.208em, jumping one step in the scale
-      in order to really stand apart. All headers have a margin of 1.5em
-      above them to guarantee them enough space, so they can be detached
-      from the former content in a consistent way.
+      The scale used for the title font sizes is the Perfect Fourth. Starting
+      with a base font size of 16px, a `h4` tag has a size of 1.333em, h3
+      1.777em, h2 2.369em and h1 4.208em, jumping one step in the scale in order
+      to really stand apart. All headers have a margin of 1.5em above them to
+      guarantee them enough space, so they can be detached from the former
+      content in a consistent way.
     </p>
     <p>
-      The font stack prioritizes performance and platform support. This is
-      why we use the font Avenir for the headings on Mac/iOS, while the standard
+      The font stack prioritizes performance and platform support. This is why
+      we use the font Avenir for the headings on Mac/iOS, while the standard
       text font is the system one.
     </p>
     <p>
@@ -32,18 +32,18 @@
 
 <script>
 export default {
-  name: 'typography',
+  name: "typography",
   data() {
     return {
-      msg: 'Typography for this site',
+      msg: "Typography for this site"
     };
-  },
+  }
 };
 </script>
 
 <style>
 .inverted {
-  overflow:auto;
+  overflow: auto;
   background-color: #2e3673;
   color: white;
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from "vue";
+import App from "./App.vue";
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App },
+  el: "#app",
+  template: "<App/>",
+  components: { App }
 });
